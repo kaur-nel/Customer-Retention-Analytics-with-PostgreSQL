aefe1dfbb6f66165ea047295fd3405ae82b4adcd
kaur-nel <bis985bis@gmail.com>
2026-07-18 18:43:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,142 @@
+# Customer Retention Analytics using SQL Cohort Analysis
+An end-to-end Data Analytics project that builds a complete ETL pipeline to measure customer retention using SQL Cohort Analysis on the UCI Online Retail (2010–2011) dataset.
+
+Instead of performing all calculations in Python, this project leverages a relational database to execute heavy analytical operations, making the workflow more scalable and closer to real-world data engineering practices.
+
+## Project Overview
+Businesses spend significant resources acquiring new customers, but **customer retention** ultimately determines long-term profitability.
+
+This project answers questions like:
+
+- Are customers returning after their first purchase?
+- How quickly does customer retention decline?
+- Are marketing efforts creating loyal customers?
+
+The pipeline performs:
+
+- Extract raw transaction data
+- lean anomalies using **Pandas**
+- Load cleaned data into **PostgreSQL**
+- Execute **SQL Cohort Analysis**
+- Calculate monthly **Retention Rates**
+- Generate a **Retention Heatmap**
+
+
+**Dataset:** [Online Retail Dataset (UCI)](https://archive.ics.uci.edu/ml/datasets/Online+Retail)
+
+## Features
+
+- End-to-End **ETL Pipeline**
+- Automated Data Cleaning using **Pandas**
+- SQL Cohort Analysis using **Common Table Expressions (CTEs)**
+- PostgreSQL Data Aggregation
+- Monthly Customer Retention Analysis
+- Retention Percentage Calculation
+- Heatmap Visualization using **Seaborn**
+- Database-first Analytical Workflow
+
+## 🛠️ Tech Stack
+
+| Technology | Purpose |
+|------------|---------|
+| **Python** | ETL Pipeline |
+| **Pandas** | Data Cleaning |
+| **PostgreSQL** | Database |
+| **SQLAlchemy** | Database Connectivity |
+| **SQL (CTEs)** | Cohort Analysis |
+| **Matplotlib** | Data Visualization |
+| **Seaborn** | Retention Heatmap |
+
+
+## Project Structure
+```text
+Customer-Retention-Analytics/
+│
+├── cohort_heatmap.png      # Generated retention heatmap
+├── main.py                 # ETL pipeline & cohort analysis
+├── online_retail.csv       # Raw dataset
+├── README.md
+└── requirements.txt
+```
+
+
+## Setup and Installation 
+
+### Clone the Repository
+
+```bash
+git clone https://github.com/yourusername/customer-retention-analysis.git
+```
+
+```bash
+cd customer-retention-analysis
+```
+
+---
+
+### Install Dependencies
+
+Using the requirements file:
+
+```bash
+pip install -r requirements.txt
+```
+
+Or install manually:
+
+```bash
+pip install pandas matplotlib seaborn sqlalchemy psycopg2
+```
+
+---
+
+### Create PostgreSQL Database
+
+```sql
+CREATE DATABASE retail_project;
+```
+
+---
+
+### Configure the Database Connection
+
+Open **main.py** and update the connection string.
+
+```python
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "postgresql+psycopg2://username:password@localhost:5432/retail_project"
+)
+```
+
+---
+
+### Run the Project
+
+```bash
+python main.py
+```
+
+The pipeline automatically:
+
+- Cleans the raw dataset
+- Loads data into PostgreSQL
+- Executes SQL Cohort Analysis
+- Calculates retention percentages
+- Generates the heatmap
+- Saves the visualization as:
+
+```text
+cohort_heatmap.png
+```
+
+### Output Screenshots
+
+#### Pivot table 
+![alt text](image-1.png)
+#### Heatmap
+![alt text](cohort_heatmap.png)
+
+
+
---
+++ main.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 1. EXTRACT: Load the messy CSV
+df = pd.read_csv('online_retail.csv', encoding='unicode_escape')
+df.columns = df.columns.str.lower()
+# 2. TRANSFORM: Clean the data
+# Drop rows with no CustomerID
+df_clean = df.dropna(subset=['customerid'])
+
+# Remove refunds/cancellations (Quantity must be greater than 0)
+df_clean = df_clean[df_clean['quantity'] > 0]
+
+# Ensure the InvoiceDate is actually treated as a Date, not text
+df_clean['invoicedate'] = pd.to_datetime(df_clean['invoicedate'])
+
+# 3. LOAD: Push it into a local SQL database
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "postgresql+psycopg2://postgres:1234@localhost:5432/retail_project"
+)
+
+df_clean.to_sql('retaildata', engine, index=False, if_exists='replace')
+
+print("ETL Complete! The clean data is now in the 'RetailData' SQL table.")
+
+# 4. Write the Cohort SQL Query
+queries = [
+    """
+    SELECT
+        customerid,
+        DATE_TRUNC('month', MIN(invoicedate)) AS acquisition_month
+    FROM retaildata
+    GROUP BY customerid
+    ORDER BY customerid;
+    """,
+
+    """
+    SELECT DISTINCT
+        customerid,
+        DATE_TRUNC('month', invoicedate) AS activity_month
+    FROM retaildata
+    ORDER BY customerid, activity_month;
+    """ ,
+    
+    """
+    WITH acquisition AS (
+        SELECT
+            customerid,
+            DATE_TRUNC('month', MIN(invoicedate)) AS cohort_month
+        FROM retaildata
+        GROUP BY customerid
+    ),
+
+    activity AS (
+        SELECT DISTINCT
+            customerid,
+            DATE_TRUNC('month', invoicedate) AS activity_month
+        FROM retaildata
+    )
+
+    SELECT
+        a.customerid,
+        a.cohort_month,
+        ac.activity_month,
+        -- Calculate the difference in months (Month Index)
+        (
+            EXTRACT(YEAR FROM ac.activity_month) * 12 +
+            EXTRACT(MONTH FROM ac.activity_month)
+        ) -
+        (
+            EXTRACT(YEAR FROM a.cohort_month) * 12 +
+            EXTRACT(MONTH FROM a.cohort_month)
+        ) AS month_index
+        FROM acquisition a
+        JOIN activity ac
+            ON a.customerid = ac.customerid
+        ORDER BY a.customerid, month_index;
+    """ ,
+    
+    """
+    WITH acquisition AS (
+        SELECT
+            customerid,
+            DATE_TRUNC('month', MIN(invoicedate)) AS cohort_month
+        FROM retaildata
+        GROUP BY customerid
+    ),
+
+    activity AS (
+        SELECT DISTINCT
+            customerid,
+            DATE_TRUNC('month', invoicedate) AS activity_month
+        FROM retaildata
+    ),
+
+    cohort_offsets AS (
+        SELECT
+            a.customerid,
+            a.cohort_month,
+            (
+                EXTRACT(YEAR FROM ac.activity_month) * 12 +
+                EXTRACT(MONTH FROM ac.activity_month)
+            ) -
+            (
+                EXTRACT(YEAR FROM a.cohort_month) * 12 +
+                EXTRACT(MONTH FROM a.cohort_month)
+            ) AS month_index
+        FROM acquisition a
+        JOIN activity ac
+            ON a.customerid = ac.customerid
+    )
+
+    -- Final Aggregation Step
+    SELECT
+        cohort_month,
+        month_index,
+        COUNT(DISTINCT customerid) AS active_users
+    FROM cohort_offsets
+    GROUP BY cohort_month, month_index
+    ORDER BY cohort_month, month_index;
+    """
+]
+
+for query in queries:
+       # Run the query and view results
+       df_results = pd.read_sql_query(query, engine)
+       print(df_results)
+       
+       
+cohort_df = df_results.copy()
+cohort_df = pd.read_sql_query(queries[-1], engine)
+
+cohort_pivot = cohort_df.pivot(
+    index='cohort_month',
+    columns='month_index',
+    values='active_users'
+)
+
+print(cohort_pivot)
+
+cohort_size = cohort_pivot[0]
+retention = cohort_pivot.divide(cohort_size, axis=0) * 100
+print(retention)
+
+plt.figure(figsize=(12, 8))
+
+sns.heatmap(
+    retention,
+    annot=True,
+    fmt=".1f",
+    cmap="YlGnBu",
+    linewidths=0.5,
+    vmin=0,
+    vmax=100
+)
+
+plt.title("Customer Retention Heatmap")
+plt.xlabel("Month Index")
+plt.ylabel("Cohort Month")
+
+plt.tight_layout()
+plt.savefig(
+    "cohort_heatmap.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+plt.show()
